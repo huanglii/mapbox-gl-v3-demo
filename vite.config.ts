93b0d58e05e1d35a5c8c51e63ac81330cc73526d
huangli <naivemap@163.com>
2024-12-21 21:32:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import externalGlobals from 'rollup-plugin-external-globals'
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,21 +25,19 @@ export default defineConfig({
   build: {
     // rollup 配置
     rollupOptions: {
-      external: ['mapbox-gl'],
-      plugins: [
-        // 避免打包和生产模式运行出错 在这里声明公共模块
-        externalGlobals({
-          //  "在项目中引入的变量名称" ："CDN包导出的名称，一般在CDN包中都是可见的"
-          'mapbox-gl': 'mapboxgl',
-        }),
-      ],
-
+      // external: ['mapbox-gl'],
+      // plugins: [
+      //   // 避免打包和生产模式运行出错 在这里声明公共模块
+      //   externalGlobals({
+      //     //  "在项目中引入的变量名称" ："CDN包导出的名称，一般在CDN包中都是可见的"
+      //     'mapbox-gl': 'mapboxgl',
+      //   }),
+      // ],
       // 分包
       // output: {
       //   manualChunks(id: any): string {
       //     if (id.includes('node_modules')) {
       //       console.log(id)
-
       //       return id.toString().split('node_modules/')[1].split('/')[0].toString()
       //     }
       //   },
